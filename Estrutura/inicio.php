26504da48924d04730dd8b052a2b79174593eea0
Andre Luis <andreluis2608@gmail.com>
2021-07-21 14:36:37 -0300

    Testando git

---
+++ Estrutura/inicio.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "ola mundo";
+
+//php7 outras tags de abertura foram descontinuadas
+//<% %>
+
+?>
+php utiliza arquitetura cliente servidor
+
+um cliete faz uma solicitação(request) para o servidor
+o servidor devolve uma resposta(response)
+
+php funciona no lado servidor e devolve HTML
+
+
+-------------------
+json e xml são padroes de resposta
