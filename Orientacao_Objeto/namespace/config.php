50500ac2b8fa301b7c9e77caa70049c5e429ccc2
Andre Luis <andreluis2608@gmail.com>
2021-08-09 09:21:17 -0300

    testes namespace

--- Orientacao_Objeto/namespace/config.php
+++ Orientacao_Objeto/namespace/config.php
@@ -3,7 +3,7 @@
 
 spl_autoload_register(function($nameClass)
 {
-    $dirClass="..".DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR."Classes";
+    $dirClass="..".DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR."Classes_PHP";
     $fileName= $dirClass.DIRECTORY_SEPARATOR.$nameClass.".php";
 
     if(file_exists($fileName))
