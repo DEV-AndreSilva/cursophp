b587bf2f950ed8636eb6d5a15a59f32f55014176
Andre Luis <andreluis2608@gmail.com>
2021-08-09 09:17:42 -0300

    classes DAO

---
+++ Banco_Dados_PHP/PDO/DAO/Class/Usuario.php
@@ -0,0 +1,88 @@
+<?php
+
+class Usuario{
+    private $idususario;
+    private $deslogin;
+    private $dessenha;
+    private $dtcadastro;
+
+    public function getIdusuario()
+    {
+        return $this->idususario;
+    }
+
+    public function setIdusuario($id)
+    {
+        $this->idususario=$id;
+    }
+
+    public function getDeslogin()
+    {
+        return $this->deslogin;
+    }
+
+    public function setDeslogin($login)
+    {
+        $this->deslogin = $login;
+    }
+    
+    public function getDessenha()
+    {
+        return $this->dessenha;
+    }
+
+    public function setDessenha($senha)
+    {
+        $this->dessenha=$senha;
+    }
+    
+    public function getDtcadastro()
+    {
+        return $this->dtcadastro;
+    }
+    public function setDtcadastro($data)
+    {
+        $this->dtcadastro=$data;
+    }
+
+    /**
+     * Pesquisa de Cadastro por ID
+     *
+     * @param integer $id
+     * @return void
+     */
+    public function loadById($id)
+    {
+        $sql= new SQL();
+
+        $result=$sql->select("select * from tb_usuarios where idusuario = :id", array(
+            ":id"=>$id
+        ));
+
+        if(count($result)>0)
+        {
+            $row=$result[0];
+            $this->setIdusuario($row['idusuario']);
+            $this->setDeslogin($row['deslogin']);
+            $this->setDessenha($row['dessenha']);
+            $this->setDtcadastro(new DateTime($row['dtcadastro']));
+        }
+
+    }
+
+    /**
+     * Função chamada quando utilizado o Echo no objeto Usuario
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        return json_encode(array(
+            "idusuario"=>$this->getIdusuario(),
+            "deslogin"=>$this->getDeslogin(),
+            "dessenha"=>$this->getDessenha(),
+            "dtcadastro"=>$this->getDtcadastro()->format("d/m/Y H:i:s")
+        ));
+    }
+
+}
