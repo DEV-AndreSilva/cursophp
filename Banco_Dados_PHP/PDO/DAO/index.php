3f011b98e052c6cdd27acf054568556e530a244a
Andre Luis <andreluis2608@gmail.com>
2021-08-18 10:56:28 -0300

    Testando metodos de busca de usuário

--- Banco_Dados_PHP/PDO/DAO/index.php
+++ Banco_Dados_PHP/PDO/DAO/index.php
@@ -1,11 +1,25 @@
 <?php
 require_once("config.php");
 
+
 //carrega 1 usuário pelo seu ID
 $obUser= new Usuario();
 $obUser->loadById(1);
 echo $obUser;
 
+
 //Carrega uma lista de Usuários
 $listaUsuarios=Usuario::getList();
-echo json_encode($listaUsuarios);
+echo $listaUsuarios;
+
+//Carrega uma lista de Usuários 
+$listaOrdenada=Usuario::search("Andre");
+echo $listaOrdenada;
+
+
+//verifica se o login do usuário é valido
+$usuario = new Usuario();
+$usuario->login("Andre",987654);
+echo $usuario;
+
+
